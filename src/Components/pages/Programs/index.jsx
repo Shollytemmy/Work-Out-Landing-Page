cf0c73d9d82687d973807866c53efb6009134618
Bunyameen <bunyameen@gmail.com>
2023-01-17 14:58:07 +0100

    fix: letter-spacing issues corrected for Programs-tittle

--- src/Components/pages/Programs/index.jsx
+++ src/Components/pages/Programs/index.jsx
@@ -30,12 +30,12 @@ export const Programs = () => {
   return (
     <section className='programs mb-[180px]' id='programs'>
       <div className="program_content">
-        <h4 className='program_title'>Not sure where to start?</h4>
+        <h4 className='program_title'>Not sure where to start? </h4>
         <p className='program_text'>Programs offer day-to-day guidance on an interactive calendar to keep you on track.</p>
         <div  className='program_map'>
           {programs.map((program) => {
             return(
-              <Card key={program.id} styles={"w-[304px] h-[190px] p-5 bg-[#323443]" }>
+              <Card key={program.id} styles={"w-[304px] h-[190px] p-5 bg-[#323443]"}>
                 <h2 className='font-semibold text-[16px] leading-6 tracking-[0.5px]'>{program.title}</h2>
                 <div className='content flex mt-[20px] justify-center items-center'>
                   <p className='program_data'>{program.content}</p> 
